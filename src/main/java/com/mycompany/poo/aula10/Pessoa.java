cde3141519d1f034ca09caa5fc6658a9ffc0eaf6
Camila Turra <turra.camila@escola.pr.gov.br>
2025-08-06 13:43:14 +0000

    aula10, modificador de acesso, parte 2

--- src/main/java/com/mycompany/poo/aula10/Pessoa.java
+++ src/main/java/com/mycompany/poo/aula10/Pessoa.java
@@ -15,7 +15,7 @@ public class Pessoa {
      */  
     public String nome; // Público, é visível em qualquer classe.
     protected int idade; // Protejido, é visível somente em classes filhas (Herança).
-    private double salário; // Privado, é visível somente dentro da própria classe.
+    private double salario; // Privado, é visível somente dentro da própria classe.
     double altura; // Friendly ou Público, apesar de não estar explicando, ele também é visível.
 
     public void exibirDados() {
@@ -23,6 +23,26 @@ public class Pessoa {
     System.out.println("Nome: " + nome);
     System.out.println("Idade: " + idade);
     System.out.println("Altura (m): " + altura);
+    }
+
+    /**
+     * Nomenclatura de métodos
+     * 
+     * Métodos/Funções indicam ações, e por padrão utilizamos verbos.
+     * 
+     * Ex.: correr, andar, caminhar ...
+     */
+    public void atribuirSalario(double valor) {
+        this.salario = valor;
+
+        System.out.println("O novo salário de " + this.nome + " é: " + this.salario);
+
+    }
+
+    public void atribuirSalario(double valor, double bonus) {
+        this.salario = valor + bonus;
+
+        System.out.println("O novo salário de " + this.nome + "é: " + this.salario + " mais um bônus de " + bonus);
     }
 }
 
